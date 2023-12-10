0f98cb9a39454be92d883fa6c56df6b7cc557ce0
ahargunyllib <billy.bpm03@gmail.com>
2023-12-10 16:47:56 +0700

    Update home page UI (generated by blackbox.ai )

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,43 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            padding: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(color: Color(0xFF191414)),
+            child: const Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                      'Musicalist',
+                          style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w700,
+                          height: 0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                
+              ]
+            )
+          )
         ),
       ),
     );
